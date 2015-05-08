ad18c58c9114ab15f1cafb4ae3317c6d7298c8ab
Nur Hardyanto <nurhardyanto@gmail.com>
2015-05-08 20:20:20 +0700

    Initial Commit
    Commit awal

---
+++ application/models/DataReservasi.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Entitas untuk menyimpan data reservasi
+ * @author MNur
+ *
+ */
+class DataReservasi extends CI_Model {
+	
+	/**
+	 * Method untuk apa
+	 * @param integer $arg1 Index
+	 * @param string $arg2 asdasd
+	 * @param array $arg3 dfsaf
+	 * 
+	 * @return sdafasffae
+	 */
+	function as_as_fsdf_dsfdsf_sf($arg1, $arg2, $arg3) {
+		$mmmAdsfSdsf = 0;
+		
+	}
+}
